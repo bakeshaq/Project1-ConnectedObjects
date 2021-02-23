8131fc4af63b527f0ae2d1f4691275ae9dbbc87e
Shaquille Baker <shaq9537@hotmail.com>
2021-02-23 12:24:42 -0500

    Update config.h

--- adafruitio_08_analog_in/config.h
+++ adafruitio_08_analog_in/config.h
@@ -3,7 +3,7 @@
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
 #define IO_USERNAME "bakeshaq"
-#define IO_KEY "aio_dyuJ25cJZxLBD230YKX4wkqa62np"
+#define IO_KEY ""
 
 /******************************* WIFI **************************************/
 
@@ -20,8 +20,8 @@
 //   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
 //   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
 
-#define WIFI_SSID "House of Baker"
-#define WIFI_PASS "9054950715"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 // uncomment the following line if you are using airlift
 // #define USE_AIRLIFT
